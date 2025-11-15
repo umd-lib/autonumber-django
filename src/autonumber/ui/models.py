c79ecdb836b844db382d09cdb7ff9649872afe17
Marc Andreu Grillo Aguilar <aguilarm@umd.edu>
2025-11-14 22:50:24 -0500

    LIBITD-2595: Fix Unique Constraint for Repository
    Wrong argument in model
    
    https://umd-dit.atlassian.net/browse/LIBITD-2595

--- src/autonumber/ui/models.py
+++ src/autonumber/ui/models.py
@@ -13,7 +13,7 @@ class Name(models.Model):
 
 
 class Repository(models.Model):
-  name = models.CharField(max_length=255, db_index=True)
+  name = models.CharField(max_length=255, unique=True)
 
   def __str__(self):
     return self.name
